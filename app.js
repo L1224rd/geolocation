70d8a97ae8b279fd98ad7ce806baa2834cddfec4
Eder Marques <eder.marques@citi.org.br>
2018-08-08 21:18:55 -0300

    Initial Commit

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
+
+app.use(express.static(`${__dirname}/views`));
+
 
 app.get('/', (req, res) => {
-    res.send('LISTENING ON PORT 3000');
+  res.sendFile('index.html');
 });
 
+app.get('/coords', (req, res) => {
+  console.log(req.query);
+  res.send('');
+});
 app.listen(process.env.PORT || 3000, () => {
-    console.log('READY');
+  console.log('READY');
 });
